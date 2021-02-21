6e80120d8e7913df616300a36a10487aa6feb9f6
Marvin Bernd <marvinbernd@Marvins-MacBook-Pro.local>
2021-02-21 23:51:23 +0100

    initialize dotnet console & xunit

---
+++ src/CryptoPortfolio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CryptoPortfolio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
